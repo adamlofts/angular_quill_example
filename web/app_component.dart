ebb3535d531e10ca4009fe1865c0cbf76e4e5d90
Adam Lofts <adam.lofts@gmail.com>
2017-09-05 14:49:06 +0100

    Upgrade to angular_quill 0.0.6 (which uses angular 4)

--- web/app_component.dart
+++ web/app_component.dart
@@ -1,14 +1,15 @@
 // Copyright (c) 2017, Adam Lofts. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'package:angular2/angular2.dart';
+import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
 import 'package:angular_quill/angular_quill.dart';
 
 @Component(
   selector: 'my-app',
   styleUrls: const ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: const [COMMON_DIRECTIVES, quillDirectives],
+  directives: const [COMMON_DIRECTIVES, NgModel, quillDirectives],
 )
 class AppComponent {
   List<String> events = [];
--- web/main.dart
+++ web/main.dart
@@ -1,7 +1,7 @@
 // Copyright (c) 2017, Adam Lofts. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'package:angular2/platform/browser.dart';
+import 'package:angular/angular.dart';
 
 import 'app_component.dart';
 
